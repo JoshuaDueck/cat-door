8c9c6f563f9fc0f3e21fb5936641b1d25ae69e7a
Joshua <joshua.dueck@gmail.com>
2023-12-13 22:36:48 -0600

    Added insomnia docs, and tried something with the datetime

--- flask-backend/app/blueprints/log_entries/routes.py
+++ flask-backend/app/blueprints/log_entries/routes.py
@@ -6,7 +6,9 @@ from app.models.log_entry import LogEntry
 
 @bp.route('/', methods=['GET'])
 def index():
-    return 'Log Entries Root'
+    # Returns a list of all log entries
+    log_entries = db.session.query(LogEntry).all()
+    return {'log_entries': [log_entry.__json__() for log_entry in log_entries]}
 
 
 @bp.route('/', methods=['POST'])
