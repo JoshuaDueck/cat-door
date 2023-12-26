5d5451043781793921107beb665006d00c2d6595
Joshua Dueck <joshua.dueck@gmail.com>
2023-12-26 10:14:00 -0600

    Added pull and updated gitignore

---
+++ pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+git checkout master
+git pull origin master
+git checkout $branch
+cd flask-backend
+flask db upgrade
+./start.sh
